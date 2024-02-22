fa681122b4f0f1405d4ee98ed311d26331357290
Anthony Vu <vua11@mcmaster.ca>
2024-02-22 16:06:10 -0500

    Styled login page, Issue: #2

--- src/client/index.py
+++ src/client/index.py
@@ -1,41 +1,21 @@
 from flask import Flask, render_template, jsonify, request, send_from_directory, redirect, url_for
-import os
-import json
 from backend import dcm_to_json
-from firebase import firebase
+import os
 
 app = Flask(__name__)
-
-firebase = firebase.FirebaseApplication('https://ai-for-chest-x-ray-default-rtdb.firebaseio.com', None)
-
-@app.route("/")
-def login():
-  result = firebase.get('/doctors', None)
-  return str(result)
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
 app.config['UPLOAD_FOLDER'] = os.path.join(current_dir, 'uploads')
 
-file_name=""
-
-# @app.route('/', methods=['GET', 'POST'])
-# def login():
-#     error = None
-#     if request.method == 'POST':
-#         if request.form['username'] != 'admin' or request.form['password'] != 'admin':
-#             error = 'Invalid Credentials. Please try again.'
-#         else:
-#             return redirect(url_for('index'))
-#     return render_template('login.html', error=error)
-
-@app.route('/doctor', methods=['GET', 'POST'])
-def doctor():
+@app.route('/', methods=['GET', 'POST'])
+def login():
+    error = None
     if request.method == 'POST':
-        # Add your login logic here
-        # If login is successful, redirect to index
-        return redirect(url_for('index'))
-    # Render the login page
-    return render_template('doctor.html')
+        if request.form['username'] != '18group' or request.form['password'] != '18group':
+            error = 'Invalid Credentials. Please try again.'
+        else:
+            return redirect(url_for('index'))
+    return render_template('login.html', error=error)
 
 @app.route("/index") 
 def index():
@@ -48,11 +28,9 @@ def upload():
         if image.filename != '':
             file_path = os.path.join(app.config['UPLOAD_FOLDER'], image.filename)
             image.save(file_path)
-            print(file_path)
             weights = "densenet121-res224-mimic_ch"
             mimix_csv = "mimic-cxr-2.0.0-chexpert.csv"
             result = dcm_to_json(file_path, weights, mimix_csv)
-            print(result)
             os.remove(file_path)
             return result
 
@@ -66,10 +44,9 @@ def fetch_image():
 
 @app.route('/deleteimage')
 def delete_image():
-     print(current_dir)
-     file_path = file_path = os.path.join(app.config['UPLOAD_FOLDER'], "image.jpg")
-     os.remove(file_path)
-     return {"true": True}
+    file_path = file_path = os.path.join(app.config['UPLOAD_FOLDER'], "image.jpg")
+    os.remove(file_path)
+    return {"true": True}
 
 if __name__ == "__main__":
     app.run(debug=True)
