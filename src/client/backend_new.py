bca7fe72bd65b3ab92d134e75c96280c28c1bd30
Anthony Vu <vua11@mcmaster.ca>
2024-03-15 22:44:45 -0400

    Colour gradience images working, Issue: #2

--- src/client/backend_new.py
+++ src/client/backend_new.py
@@ -4,7 +4,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 import numpy as np
 import matplotlib
-matplotlib.use('Agg') # mac os x error
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import csv
 from torch.optim import SGD
@@ -28,25 +28,156 @@ from pytorch_grad_cam import GradCAM
 from pytorch_grad_cam.utils.image import show_cam_on_image
 from pytorch_grad_cam.utils.model_targets import ClassifierOutputTarget
 from tabulate import tabulate
-
-from flask import Flask
 from datetime import datetime
-from PIL import Image
 import torchxrayvision as xrv
-import numpy as np
-import cv2 as cv
 import skimage
-import torch
 import torchvision
-import os
 import pydicom
-import json
 
 app = Flask(__name__)
 current_dir = os.path.dirname(os.path.abspath(__file__))
 app.config['UPLOAD_FOLDER'] = os.path.join(current_dir, 'uploads')
-checkpoint_path = "/Users/yuvra/Desktop/CAPSTONE/ai-for-chest-x-ray/src/client/bce_masked_adam8.pth.tar"
+# checkpoint_path = "/Users/yuvra/Desktop/CAPSTONE/ai-for-chest-x-ray/src/client/bce_masked_adam8.pth.tar"
+checkpoint_path = "/Users/ant.vu/Developer/ai-for-chest-x-ray/src/client/bce_masked_adam8.pth.tar"
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+def calculate_age(birth_date, current_date):
+    # Calculation
+    years = current_date.year - birth_date.year
+    months = current_date.month - birth_date.month
+    days = current_date.day - birth_date.day
+    # Adjust for negative differences
+    if days < 0:
+        months -= 1
+        days += get_days_in_month(birth_date.month, birth_date.year)
+    if months < 0:
+        years -= 1
+        months += 12
+    return years, months, days
+
+def get_days_in_month(month, year):
+    # Returns the number of days in a given month and year
+    if month == 2:  # February
+        if year % 4 == 0 and (year % 100 != 0 or year % 400 == 0):
+            return 29  # Leap year
+        else:
+            return 28
+    elif month in [4, 6, 9, 11]:  # April, June, September, November
+        return 30
+    else:
+        return 31
+
+def dcm_patient_data(path_file):
+    dcm_data = pydicom.dcmread(path_file)
+    patient_data = {}
+    have_patient_dob = False
+    have_acquisition_date = False
+    patient_birth_date = ""
+    patient_age_now = ""
+    acquisition_date = ""
+    acquisition_age = ""
+    patient_data.update({"Study_ID": str(dcm_data.StudyID)})
+    patient_data.update({"Patient_ID": str(dcm_data.PatientID)})
+    patient_data.update({"Patient_Name": str(dcm_data.PatientName)})
+    patient_data.update({"Patient_Sex": str(dcm_data.PatientSex)})
+    if dcm_data.PatientBirthDate == "":
+        patient_birth_date = ""
+        patient_age_now = ""
+    else:
+        have_patient_dob = True
+        birth_date = datetime.utcfromtimestamp(int(dcm_data.PatientBirthDate))
+        if birth_date <= datetime.now():
+            age_years, age_months, age_days = calculate_age(birth_date, datetime.now())
+            patient_age_now = f"{age_years} years, {age_months} months, {age_days} days"
+            patient_birth_date = birth_date.strftime('%Y-%m-%d')
+    if dcm_data.AcquisitionDate == "":
+        acquisition_date = ""
+        acquisition_age = ""
+    else:
+        have_acquisition_date = True
+        acquisition_date = datetime.utcfromtimestamp(int(dcm_data.AcquisitionDate))
+        patient_acquisition_date = acquisition_date.strftime('%Y-%m-%d')
+    if have_patient_dob and have_acquisition_date:
+        if birth_date <= acquisition_date:
+            age_years, age_months, age_days = calculate_age(birth_date, acquisition_date)
+            acquisition_age = f"{age_years} years, {age_months} months, {age_days} days"
+    patient_data.update({"Patient_Birth_Date": str(patient_birth_date)})
+    patient_data.update({"Patient_Current_Age": str(patient_age_now)})
+    patient_data.update({"Series_Number": str(dcm_data.SeriesNumber)})
+    patient_data.update({"Acquisition_Number": str(dcm_data.AcquisitionNumber)})
+    patient_data.update({"Acquisition_Date": str(patient_acquisition_date)})
+    patient_data.update({"Instance_Number": str(dcm_data.InstanceNumber)})
+    patient_data.update({"View_Position": str(dcm_data.ViewPosition)})
+    patient_data.update({"Patient_Orientation": str(dcm_data.PatientOrientation)})
+    patient_data.update({"Patient_Age_at_Time_of_Acquisition": str(acquisition_age)})
+    return patient_data
+
+def run_model(file_path, weight_string):
+    img = skimage.io.imread(file_path)
+    img = xrv.datasets.normalize(img, 255)  # convert 8-bit image to [-1024, 1024] range
+    img = img.mean(2)[None, ...]  # Make single color channel
+    transform = torchvision.transforms.Compose([xrv.datasets.XRayCenterCrop(), xrv.datasets.XRayResizer(224)])
+    img = transform(img)
+    img = torch.from_numpy(img)
+    model = xrv.models.DenseNet(weights=weight_string)
+    outputs = model(img[None, ...])
+    probability_dictionary = dict(zip(model.pathologies, outputs[0].detach().numpy()))
+    model_dict = {}
+    model_dict.update({"Model_Atelectasis": round(float(probability_dictionary['Atelectasis'])*100, 2)})
+    model_dict.update({"Model_Cardiomegaly": round(float(probability_dictionary['Cardiomegaly'])*100, 2)})
+    model_dict.update({"Model_Consolidation": round(float(probability_dictionary['Consolidation'])*100, 2)})
+    model_dict.update({"Model_Edema": round(float(probability_dictionary['Edema'])*100, 2)})
+    model_dict.update({"Model_Effusion": round(float(probability_dictionary['Effusion'])*100, 2)})
+    return model_dict
+
+def actual_dcm_dict(file_path, study_id):
+    line_content = []
+    with open(file_path) as myFile:
+        for _, line in enumerate(myFile, 0):
+            if study_id in line:
+                line_content = line.split(",")
+    Atelectasis_index = 2
+    Cardiomegaly_index = 3
+    Consolidation_index = 4
+    Edema_index = 5
+    Pleural_Effusion_index = 11
+    actual_dictionary = {
+        'Actual_Atelectasis': 0,
+        'Actual_Cardiomegaly': 0,
+        'Actual_Consolidation': 0,
+        'Actual_Edema': 0,
+        'Actual_Effusion': 0
+    }
+    if line_content[Atelectasis_index] == "1.0":
+        actual_dictionary['Actual_Atelectasis'] = 1
+    if line_content[Cardiomegaly_index] == "1.0":
+        actual_dictionary['Actual_Cardiomegaly'] = 1
+    if line_content[Consolidation_index] == "1.0":
+        actual_dictionary['Actual_Consolidation'] = 1
+    if line_content[Edema_index] == "1.0":
+        actual_dictionary['Actual_Edema'] = 1
+    if line_content[Pleural_Effusion_index] == "1.0":
+        actual_dictionary['Actual_Effusion'] = 1
+    return actual_dictionary
+
+def create_json(original_dict):
+    new_dict = {}
+    for x in original_dict:
+        new_dict.update(x)
+        new_dict.update(x)
+        new_dict.update(x)
+    with open("output.json", "w") as out_file:
+        json.dump(new_dict, out_file, indent=4)
+    return json.dumps(new_dict, indent=4)
+
+def dcm_to_json(file_path, weight_string, csv_file_path):
+    convert_dcm_to_jpg(file_path)
+    patient_data = dcm_patient_data(file_path)
+    actual_tags = actual_dcm_dict(csv_file_path, patient_data["Study_ID"])
+    file_path = os.path.join(app.config['UPLOAD_FOLDER'], "scaled_image.jpg")
+    model_output = run_model(file_path, weight_string)
+    json_value = create_json([patient_data, model_output, actual_tags])
+    return json_value
 
 def create_densenet121(num_classes):
     # Load the DenseNet121 model without pre-trained weights
@@ -234,15 +365,12 @@ def run_with_no_csv(filepath):
     loaded_model, loaded_optimizer = local_load_checkpoint(model, optimizer,device)
     loaded_model.eval()
     loaded_model.to(device)
-    #filepath = "/Users/abhisheksharma/Downloads/local_model (copy)/images/8c421366-9dbe0c06-c9eb74e9-ba9a1e34-f39d6739.jpg"
-    #csv_file_path = "/Users/abhisheksharma/Downloads/local_model (copy)/Validation_Partial.csv"
     #thresholds are old and determine if model predictions are 1 or 0 based on model probability
     thresholds = [0.53880334, 0.48418066, 0.36754248, 0.5815063, 0.54026645, 0.47844747]
     diseaseNames = ["Atelectasis", "Cardiomegaly", "Consolidation", "Edema", "No Finding", "Pleural Effusion"]
     print("thresholds: ", thresholds)
     #is you use test_single_image or test_single_image_no_csv depends on if you have the true labels
     original_image, grad_cam_image, predictions = test_single_image_no_csv(filepath, thresholds, model, device)
-    #original_image, grad_cam_image = test_single_image_no_csv(filepath, thresholds, model, device)
     for x in range(0,6):
         plot_images(original_image, grad_cam_image[x], diseaseNames[x])
     grad_cam_images_base64 = [image_to_base64(img) for img in grad_cam_image]
@@ -268,68 +396,4 @@ def convert_dcm_to_jpg(file_path):
     final_image = Image.fromarray(scaled_image)
     file_path = os.path.join(app.config['UPLOAD_FOLDER'], "image.jpg")
     final_image.save(file_path)
-    # image = cv.imread(file_path)
-    # RGB = cv.cvtColor(image, cv.COLOR_BGR2RGB)
-    # new_image = Image.fromarray(RGB)
-    # new_image = new_image.resize((1024, 1024))
-    # file_path = os.path.join(app.config['UPLOAD_FOLDER'], "scaled_image.jpg")
-    # image.save(file_path)
     return
-
-# result = run_with_no_csv("/Users/ant.vu/Developer/ai-for-chest-x-ray/src/demo_images/8c0171a3-925313ff-f63faed5-3007b5ad-d1bbb676.jpg")
-# print(result)
-# result2 = json.loads(result)
-# print(result2)
-# # Test Single Image:
-# model = create_densenet121(6)
-# optimizer = torch.optim.Adam(model.parameters(), 0.001)
-# loaded_model, loaded_optimizer = local_load_checkpoint(model, optimizer,device)
-# loaded_model.eval()
-# loaded_model.to(device)
-# filepath = "uploads/8c0171a3-925313ff-f63faed5-3007b5ad-d1bbb676.jpg"
-# csv_file_path = "Validation_Partial.csv"
-# #thresholds are old and determine if model predictions are 1 or 0 based on model probability
-# thresholds = [0.53880334, 0.48418066, 0.36754248, 0.5815063, 0.54026645, 0.47844747]
-# diseaseNames = ["Atelectasis", "Cardiomegaly", "Consolidation", "Edema", "No Finding", "Pleural Effusion"]
-# print("thresholds: ", thresholds)
-# #you use test_single_image or test_single_image_no_csv depends on if you have the true labels
-# # original_image, grad_cam_image, predictions = test_single_image(filepath, csv_file_path, thresholds, model, device)
-# original_image, grad_cam_image, predictions = test_single_image_no_csv(filepath, thresholds, model, device)
-# # for x in range(0,6):
-# #     plot_images(original_image, grad_cam_image[x], diseaseNames[x])
-# grad_cam_images_base64 = [image_to_base64(img) for img in grad_cam_image]
-# diseases_data = []
-# for i, disease_name in enumerate(diseaseNames):
-#     diseases_data.append({
-#         "diseaseName": disease_name,
-#         "prediction": predictions[i],
-#         "gradCamImage": grad_cam_images_base64[i]
-#     })
-# data = {
-#     "diseasesData": diseases_data
-# }
-# # Write the structured data to a JSON file
-# with open('diseases_predictions_and_images.json', 'w') as json_file:
-#     json.dump(data, json_file, indent=4)
-
-# # Test Preprocessing and Byte Array Conversion:
-# image_path = "uploads/8c0171a3-925313ff-f63faed5-3007b5ad-d1bbb676.jpg"
-# # Example usage
-# byte_array = preprocess_and_convert_to_byte_array(image_path)
-# # Convert byte array back to image
-# restored_image = byte_array_to_image(byte_array)
-# # Plotting preprocessed and restored images side by side
-# preprocessed_image = Image.open(image_path)  # Re-open the original image for comparison
-# # Create a figure to display the images
-# plt.figure(figsize=(10, 5))
-# # Plot preprocessed image
-# plt.subplot(1, 2, 1)  # 1 row, 2 columns, 1st subplot
-# plt.imshow(preprocessed_image)
-# plt.title('Preprocessed Image')
-# plt.axis('off')  # Hide axis
-# # Plot restored image
-# plt.subplot(1, 2, 2)  # 1 row, 2 columns, 2nd subplot
-# plt.imshow(restored_image)
-# plt.title('Restored Image')
-# plt.axis('off')  # Hide axis
-# plt.show()
